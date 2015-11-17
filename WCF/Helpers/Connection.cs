2e7e53df8c900c252cc94d3d1576dcafee87336d
VanOxy <ivanokulichev@gmail.com>
2015-11-17 15:11:18 +0100

    Get connection string from Web.config file via helper class

---
+++ WCF/Helpers/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WCF.Helpers
+{
+    public class Connection
+    {
+        private string _connectionString;
+
+        public Connection()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["UnivercityDatabase"].ConnectionString;
+        }
+
+        public string GetConnectionString()
+        {
+            return _connectionString;
+        }
+    }
+}
---
+++ WCF/Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCF.Model
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
