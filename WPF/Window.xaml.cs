af22beb2159192a9712dc09f21e6fda970f82f30
VanOxy <ivanokulichev@gmail.com>
2015-12-12 16:47:00 +0100

    Theme ModernUI imported

--- WPF/Window.xaml.cs
+++ WPF/Window.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Windows;
 
 namespace WPF
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : ModernWindow
     {
         public MainWindow()
         {
