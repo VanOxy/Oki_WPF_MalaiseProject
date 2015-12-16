38954393b2c4b2b5c798aa174725b85b0b0b8184
VanOxy <ivanokulichev@gmail.com>
2015-12-16 16:40:47 +0100

    Pass the parameter through command (mvvm). DataGrid binding

--- WPF/ViewModel/StudentsViewModel.cs
+++ WPF/ViewModel/StudentsViewModel.cs
@@ -2,6 +2,7 @@
 using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,61 @@ using WPF.Message;
 
 namespace WPF.ViewModel
 {
+    public class User
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
     public class StudentsViewModel
     {
         public StudentsViewModel()
         {
+            // assign commands
             ChangeToMainPageCommand = new RelayCommand(ChangeToMainPage);
+            AddStudentCommand = new RelayCommand<User>(AddUser);
+
+            // test
+            FillCollection();
         }
+
+        private void AddUser(User user)
+        {
+            if (user != null)
+                Collection.Remove(user);
+        }
+
+        #region Commands
+
+        public RelayCommand<User> AddStudentCommand { get; set; }
+
+        #endregion Commands
+
+        #region Data
+
+        public ObservableCollection<User> Collection { get; set; }
+
+        #endregion Data
+
+        #region Methods
+
+        /// <summary>
+        /// Test purposes --> delete at the end !!!!! ************************************
+        /// </summary>
+        private void FillCollection()
+        {
+            Collection = new ObservableCollection<User>();
+            for (int i = 12; i < 50; i++)
+            {
+                Collection.Add(new User()
+                {
+                    Age = i,
+                    Name = "name " + i
+                });
+            }
+        }
+
+        #endregion Methods
 
         #region Navigation
 
