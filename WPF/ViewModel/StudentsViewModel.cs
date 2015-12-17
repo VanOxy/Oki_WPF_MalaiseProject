a764ddf1d462bcd41f67a27a1e7ffe70137fa400
VanOxy <ivanokulichev@gmail.com>
2015-12-17 12:39:33 +0100

    Search almost done

--- WPF/ViewModel/StudentsViewModel.cs
+++ WPF/ViewModel/StudentsViewModel.cs
@@ -6,8 +6,10 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
 using WPF.Message;
 
 namespace WPF.ViewModel
@@ -28,10 +30,13 @@ namespace WPF.ViewModel
             RemoveStudentCommand = new RelayCommand<User>(DeleteStudent);
             ModifyStudentCommand = new RelayCommand<User>(ModifyStudent);
 
+            // collections
+            DynamicCollection = new ObservableCollection<User>();
+            UsersCollection = new ObservableCollection<User>();
+
             // test
             FillCollection();
-
-            FillUserPropertiesCollection(Collection.First());
+            FillUserPropertiesCollection(DynamicCollection.First());
         }
 
         #region Commands
@@ -44,25 +49,9 @@ namespace WPF.ViewModel
 
         #region Data
 
-        public ObservableCollection<User> Collection { get; set; }
+        public ObservableCollection<User> DynamicCollection { get; set; }
         public ObservableCollection<string> UserProperties { get; set; }
-
-        private string _searchTextbox = "";
-
-        public string SearchTextbox
-        {
-            get { return _searchTextbox; }
-            set
-            {
-                if (value != _searchTextbox)
-                {
-                    _searchTextbox = value;
-                    RaisePropertyChanged("SearchTextbox");
-
-                    PerformResearch();
-                }
-            }
-        }
+        public ObservableCollection<User> UsersCollection { get; set; }
 
         #endregion Data
 
@@ -81,7 +70,7 @@ namespace WPF.ViewModel
         private void DeleteStudent(User std)
         {
             if (std != null)
-                Collection.Remove(std);
+                DynamicCollection.Remove(std);
         }
 
         /// <summary>
@@ -89,20 +78,123 @@ namespace WPF.ViewModel
         /// </summary>
         private void FillCollection()
         {
-            Collection = new ObservableCollection<User>();
             for (int i = 12; i < 50; i++)
             {
-                Collection.Add(new User()
+                UsersCollection.Add(new User()
                 {
                     Age = i,
                     Name = "name " + i
                 });
             }
+
+            foreach (var item in UsersCollection)
+                DynamicCollection.Add(item);
         }
 
         #endregion CRUD
 
         #region Search
+
+        private bool _timeOut = true;
+        private int _counter = 2;
+        private string _searchTextbox = "";
+
+        public string SearchTextbox
+        {
+            get { return _searchTextbox; }
+            set
+            {
+                if (value != _searchTextbox)
+                {
+                    _searchTextbox = value;
+                    RaisePropertyChanged("SearchTextbox");
+
+                    PerformResearch();
+                }
+            }
+        }
+
+        private string _searchCriteria;
+
+        public string SearchCriteria
+        {
+            get { return _searchCriteria; }
+            set
+            {
+                if (_searchCriteria != value)
+                {
+                    _searchCriteria = value;
+                    RaisePropertyChanged("SearchCriteria");
+                }
+            }
+        }
+
+        private void PerformResearch()
+        {
+            // algo pout faire un delai après que l'user aie tapé le txt dans la search box.
+            // Et eviter de refaire le tri à chaque capture d'event
+            Task.Run(() =>
+            {
+                if (_timeOut)
+                {
+                    _timeOut = false;
+
+                    while (!_timeOut)
+                    {
+                        if (_counter != 0)
+                        {
+                            Thread.Sleep(750);
+                            _counter--;
+                        }
+                        else
+                        {
+                            SearchAndFillDynamicCollection();
+                            _timeOut = true;
+                        }
+                    }
+                }
+                _counter = 2;
+            });
+        }
+
+        /// <summary>
+        /// Method which will perform a research based on what user enter into Searchbox
+        /// and what parameter he wants to search.
+        /// </summary>
+        private void SearchAndFillDynamicCollection()
+        {
+            if (_searchTextbox == "")
+            {
+                App.Current.Dispatcher.Invoke((Action)(() =>
+                {
+                    foreach (var item in UsersCollection)
+                    {
+                        DynamicCollection.Add(item);
+                    }
+                }));
+            }
+            else
+            {
+                App.Current.Dispatcher.Invoke((Action)(() =>
+                {
+                    DynamicCollection.Clear();
+                }));
+
+                var query = from u in UsersCollection
+                            where u.Name.Contains(_searchTextbox)
+                            select u;
+
+                var tempList = query.ToList();
+
+                App.Current.Dispatcher.Invoke((Action)(() =>
+                {
+                    foreach (var item in tempList)
+                    {
+                        DynamicCollection.Add(item);
+                    }
+                }));
+            }
+        }
 
         /// <summary>
         /// Remplit la collection UserProperties avec les noms des proprietes du type User.
@@ -112,15 +204,14 @@ namespace WPF.ViewModel
         {
             UserProperties = new ObservableCollection<string>();
 
-            obj.GetType().GetProperties().ToList().ForEach(property =>
+            var propertiesList = obj.GetType().GetProperties().ToList();
+
+            propertiesList.ForEach(property =>
             {
                 UserProperties.Add(property.Name);
             });
-        }
 
-        private void PerformResearch()
-        {
-            throw new NotImplementedException();
+            _searchCriteria = propertiesList.First().Name;
         }
 
         #endregion Search
